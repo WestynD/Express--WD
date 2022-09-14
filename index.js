6c32ae473334faabff39a6b80a1a2fffe6eb3072
WestynD <70765092+WestynD@users.noreply.github.com>
2022-09-14 15:17:52 -0600

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log('Loading Server')
 const PORT = 3000
+const db = require('./db.json')
 
 const express = require('express')
 
@@ -9,23 +10,35 @@ const app = express()
 
 //REST calls
 //Create
-app.post('', (req, res) => {
-  console.log('Creating new log') // TODO: fill in path and make function
-})
+//'/logs?courseid=cs4690&uvuid=101111111
+// app.post('/user', (req, res) => {
+//   console.log('Creating new log') // TODO: fill in path and make function
+// })
 
-//Read
-app.get('/', (req, res) => {
-  res.send('Getting log') // TODO: fill in path and make function
+//Read // /api/v1/logs?courseId=cs4690&uvuId=10111111
+app.get('/api/v1/logs', (req, res) => {
+  const courseId = req.params('courseId')
+  const uvuId = req.params('uvuId')
+  //res.send(`Get logs ${courseId} ${uvuId}`)
+  // get data from JSON
+  // filter for what I need
+  // send it back
 })
 
 //Update put(client id, full replace), patch(partial replace)
+// app.put('/user', (req, res) => {
+//   res.send('Got a PUT request at /user')
+// })
 
 //Delete
+// app.delete('/user', (req, res) => {
+//   res.send('Got a DELETE request at /user')
+// })
 
 //List all
-app.get('', (req, res) => {
-  res.send('Getting all logs') // TODO: fill in path and make function
-})
+// app.get('', (req, res) => {
+//   res.send('Getting all logs') // TODO: fill in path and make function
+// })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on Port ${PORT}`)
